bab23aed6ed1a132a481e27a9d2701a0b354ff42
LuisaZ <luisaziccarelli@gmail.com>
2020-03-04 13:09:14 +1100

    webhook set up succesfull

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   resources :listings
   get "/payments/success", to: "payments#success"
+  
+  post "/payments/webhook", to: "payments#webhook"
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "/", to: "pages#home", as: "root"
   get "*path", to: "pages#not_found"
