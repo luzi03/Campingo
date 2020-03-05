4b06230e05179dce27df0b88aabf17979ace326f
LuisaZ <luisaziccarelli@gmail.com>
2020-03-05 13:27:10 +1100

    deleted scaffold profiles

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
  
   devise_for :users
-  resources :profiles
   resources :listings
   resources :users 
   
--- db/schema.rb
+++ db/schema.rb
@@ -68,18 +68,6 @@ ActiveRecord::Schema.define(version: 2020_03_05_014401) do
     t.index ["user_id"], name: "index_orders_on_user_id"
   end
 
-  create_table "profiles", force: :cascade do |t|
-    t.bigint "user_id"
-    t.string "first_name"
-    t.string "surname"
-    t.string "location"
-    t.string "phone"
-    t.text "bio"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_profiles_on_user_id"
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -101,5 +89,4 @@ ActiveRecord::Schema.define(version: 2020_03_05_014401) do
   add_foreign_key "listings", "categories"
   add_foreign_key "orders", "listings"
   add_foreign_key "orders", "users"
-  add_foreign_key "profiles", "users"
 end
