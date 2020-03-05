342fc793bd7fc8a45e538f9c4061134bb5240d8e
LuisaZ <luisaziccarelli@gmail.com>
2020-03-06 10:32:55 +1100

    bug on devise when added username

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,8 +5,8 @@ class ApplicationController < ActionController::Base
     protected
 
     def configure_permitted_parameters
-        devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :location, :phone, :email, :password, :remember_me])
+        devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :user_name, :location, :email, :password, :remember_me])
         devise_parameter_sanitizer.permit(:sign_in, keys:[:email, :password, :remember_me])
-        devise_parameter_sanitizer.permit(:account_update, keys:[:first_name, :last_name, :location, :phone, :bio, :email, :password, :remember_me])
+        devise_parameter_sanitizer.permit(:account_update, keys:[:first_name, :last_name, :user_name, :location, :phone, :bio, :email, :password, :remember_me])
     end
 end 
