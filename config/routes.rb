d7fbbd35edbb4ee26e992bfccd95ae8d9e97d439
LuisaZ <luisaziccarelli@gmail.com>
2020-03-08 13:00:30 +1100

    links fixed

--- config/routes.rb
+++ config/routes.rb
@@ -15,10 +15,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "/", to: "pages#home", as: "root"
   
-<<<<<<< HEAD
   get "/:path", to: "pages#not_found"
-=======
-  get "path", to: "pages#not_found"
->>>>>>> messages
 
 end
